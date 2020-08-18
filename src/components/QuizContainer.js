1b778bc9cac7d1fa17e900fd49f0f2ab17e833f4
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-08-17 21:03:59 -0400

    data + QuizContainer

--- src/components/QuizContainer.js
+++ src/components/QuizContainer.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+//import { Link } from 'react-router-dom';
+import { Data } from '../data/data.js';
+//import { findAllByTitle } from '@testing-library/react';
 
 function QuizContainer(){
+
+  const [count, setCount] = useState(Data[0].questions);
+  console.log(count);
+
+  const allAnswers = count[0].incorrectAnswers.push(count[0].correctAnswer);
+  console.log(count[0].incorrectAnswers)
+  console.log(count[0].correctAnswer)
+  console.log(allAnswers);
+  
   return (
       <div>
           <h1>Quiz Container Here</h1>
+          <p> {count[0].text} </p>
+          <div>{count[0].correctAnswer} </div>
+          <div></div>
+          <div></div>
+          <div></div>
       </div>
   )
 };
