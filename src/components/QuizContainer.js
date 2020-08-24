a61475439fbe39cb892eb1a7cd0fc6f94ea9ca64
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-08-23 23:37:43 -0400

    redux need to fix

--- src/components/QuizContainer.js
+++ src/components/QuizContainer.js
@@ -2,27 +2,31 @@ import React, { useState } from 'react';
 //import { Link } from 'react-router-dom';
 import { Data } from '../data/data.js';
 //import { findAllByTitle } from '@testing-library/react';
+import { connect } from 'react-redux';
 
 function QuizContainer(){
 
-  const [count, setCount] = useState(Data[0].questions);
-  console.log(count);
-
-  const allAnswers = count[0].incorrectAnswers.push(count[0].correctAnswer);
-  console.log(count[0].incorrectAnswers)
-  console.log(count[0].correctAnswer)
+  const [localstate, setLocalstate] = useState('');
   console.log(allAnswers);
   
   return (
       <div>
           <h1>Quiz Container Here</h1>
-          <p> {count[0].text} </p>
-          <div>{count[0].correctAnswer} </div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <p> </p>
       </div>
   )
 };
 
-export default QuizContainer;
+const mapStateToProps = state => {
+  return {
+      ctr: localstate.title
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+      onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuizContainer);
